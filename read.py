a1e0d200caa64a18e723e7c4c6c31297755d0f4b
Casper Hsu <casperi52400@gmail.com>
2021-03-09 15:40:52 +0800

    Finish

--- read.py
+++ read.py
@@ -4,6 +4,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-print(data[0])
+		if len(line) % 1000 == 0:
+				print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print('留言平均長度為'sum_len/len(data))
